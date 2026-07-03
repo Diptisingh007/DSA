ca1af0a15c3fdcae6cdb6e632505ea9fc6ada11b
Diptisingh007 <dipptisingh001@gmail.com>
2026-07-03 22:04:45 +0530

    Time: 0 ms (100%), Space: 10.8 MB (70.68%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    // int characterReplacement(string s, int k) {
+    //     int n=s.size();
+    //     int l=0,r=0;
+    //     int maxlen=0,maxfreq=0;
+    //     vector<int> freq(26,0);
+
+    //     while(r<n){
+    //         freq[s[r]-'A']++;
+    //         maxfreq=max(maxfreq,freq[s[r]-'A']);
+
+    //         int len=r-l+1;
+    //         while((len-maxfreq)>k){
+    //             freq[s[l]-'A']--;
+    //             maxfreq=0;
+    //             for(int i=0;i<26;i++){
+    //                 maxfreq=max(maxfreq,freq[i]);
+    //             }
+    //             l++;
+    //             len=r-l+1;
+    //         }
+
+    //         maxlen=max(maxlen,len);
+    //         r++;
+    //     }
+    //     return maxlen;
+    // }
+
+
     int characterReplacement(string s, int k) {
         int n=s.size();
         int l=0,r=0;
@@ -13,14 +41,15 @@ public:
             int len=r-l+1;
             if((len-maxfreq)>k){
                 freq[s[l]-'A']--;
-                maxfreq=0;
-                for(int i=0;i<26;i++){
-                    maxfreq=max(maxfreq,freq[i]);
-                }
+                // maxfreq=0;
+                // for(int i=0;i<26;i++){
+                //     maxfreq=max(maxfreq,freq[i]);
+                // }
                 l++;
                 len=r-l+1;
             }
-
+            
+            if((len-maxfreq)<=k)
             maxlen=max(maxlen,len);
             r++;
         }
