bd246f5d3249be6077a248740d510e7a91533c1e
Diptisingh007 <dipptisingh001@gmail.com>
2026-07-03 13:49:05 +0530

    Time: 0 ms (100%), Space: 12.9 MB (68.87%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -17,18 +17,34 @@ public:
     //     return solve(0,0,arr,dp);
     // }
 
+    // int minimumTotal(vector<vector<int>>& arr) {
+    //     int n= arr.size();
+    //     vector<vector<int>> dp(n,vector<int>(n,0));
+    //     for(int j=0;j<n;j++){
+    //         dp[n-1][j]=arr[n-1][j];
+    //     }
+
+    //     for(int i=n-2;i>=0;i--){
+    //         for(int j=0;j<=i;j++){
+    //             dp[i][j]=arr[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+
+
     int minimumTotal(vector<vector<int>>& arr) {
         int n= arr.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> dp(n,0);
         for(int j=0;j<n;j++){
-            dp[n-1][j]=arr[n-1][j];
+            dp[j]=arr[n-1][j];
         }
 
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                dp[i][j]=arr[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+                dp[j]=arr[i][j] + min(dp[j],dp[j+1]);
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
