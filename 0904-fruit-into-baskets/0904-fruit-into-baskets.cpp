c78373cd9e98d4f3fb6c3123bb3bd6d64294d484
Diptisingh007 <dipptisingh001@gmail.com>
2026-07-03 21:25:57 +0530

    Time: 47 ms (43.02%), Space: 88.8 MB (7.5%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -7,7 +7,7 @@ public:
         map<int,int> mpp;
         while(r<n){
             mpp[nums[r]]++;
-            while(l<r && mpp.size()>2){
+            if(l<r && mpp.size()>2){
                 mpp[nums[l]]--;
                 if(mpp[nums[l]]==0){
                     mpp.erase(nums[l]);
