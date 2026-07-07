bc8786930f3f0d7a9ec62b8b36b14a3ab3f5031c
Diptisingh007 <dipptisingh001@gmail.com>
2026-07-07 21:32:49 +0530

    Time: 43 ms (22.34%), Space: 47.8 MB (10.85%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int m=tasks.size();
+        map<char,int> mpp;
+        for(auto &it: tasks){
+            mpp[it]++;
+        }
+
+        priority_queue<int> pq;
+        for(auto &it: mpp) pq.push(it.second);
+        
+        int ans=0;
+        while(!pq.empty()){
+            vector<int> temp;
+            for(int i=0;i<n+1;i++){
+                if(!pq.empty()){
+                    int freq=pq.top();
+                    pq.pop();
+                    freq--;
+                    temp.push_back(freq);
+                }
+            }
+
+            for(auto &it: temp){
+                if(it>0) pq.push(it);
+            }
+
+            if(!pq.empty()){
+                ans+=(n+1);
+            }
+            else{
+                ans+=temp.size();
+            }
+        }
+        return ans;
+    }
+};
