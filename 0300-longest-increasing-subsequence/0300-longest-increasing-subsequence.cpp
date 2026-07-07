2020de7bd4f19255d6ac8590f66b04a77a3f01de
Diptisingh007 <dipptisingh001@gmail.com>
2026-07-07 15:59:59 +0530

    Time: 60 ms (62.41%), Space: 14.3 MB (61.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -21,19 +21,37 @@ public:
 
 
 
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<vector<int>> dp(n+1,vector<int>(n+1,0));
 
-        for(int i=n-1;i>=0;i--){
-            for(int prev=i-1;prev>=-1;prev--){
-                int len=0+dp[i+1][prev+1];
-                if(prev==-1 || nums[i]>nums[prev]){
-                    len=max(len,1+dp[i+1][i+1]);
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int prev=i-1;prev>=-1;prev--){
+    //             int len=0+dp[i+1][prev+1];
+    //             if(prev==-1 || nums[i]>nums[prev]){
+    //                 len=max(len,1+dp[i+1][i+1]);
+    //             }
+    //             dp[i][prev+1]=len;
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+
+
+
+    int lengthOfLIS(vector<int>& nums){
+        int n=nums.size();
+        vector<int> dp(n,1);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[i]>nums[prev]){
+                    dp[i]=max(dp[i],1+dp[prev]);
                 }
-                dp[i][prev+1]=len;
             }
-        }
-        return dp[0][0];
+            ans=max(ans,dp[i]);
+        } 
+        return ans;
     }
+
 };
