c6abfb93937f0afcd8c9e9c6fe1c1724dd4ab136
Diptisingh007 <dipptisingh001@gmail.com>
2026-07-12 13:48:59 +0530

    Time: 35 ms (88.64%), Space: 39.9 MB (83.39%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-    vector<int> arrayRankTransform(vector<int>& arr) {
-        int n=arr.size();
-        map<int,int> mpp;
-        vector<int> nums=arr;
-        sort(nums.begin(),nums.end());
-        int rank=1;
-        for(int i=0;i<n;i++){
-            if(mpp.find(nums[i])==mpp.end()){
-                mpp[nums[i]]=rank++;
-            }
-        }
+    // vector<int> arrayRankTransform(vector<int>& arr) {
+    //     int n=arr.size();
+    //     unordered_map<int,int> mpp;
+    //     vector<int> nums=arr;
+    //     sort(nums.begin(),nums.end());
+    //     int rank=1;
+    //     for(int i=0;i<n;i++){
+    //         if(mpp.find(nums[i])==mpp.end()){
+    //             mpp[nums[i]]=rank++;
+    //         }
+    //     }
          
+    //     for(int i=0;i<n;i++){
+    //         nums[i]=mpp[arr[i]];
+    //     }
+
+    //     return nums;
+    // }
+
+
+    vector<int> arrayRankTransform(vector<int>& arr){
+        int n=arr.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         for(int i=0;i<n;i++){
-            nums[i]=mpp[arr[i]];
+            pq.push({arr[i],i});
         }
 
-        return nums;
+        vector<int> ans(n);
+        int rank=1;
+
+        while(!pq.empty()){
+            auto it=pq.top();
+            int val=it.first;
+            int index=it.second;
+
+            ans[index]=rank;
+
+            while(!pq.empty() && pq.top().first==val){
+                ans[pq.top().second]=rank;
+                pq.pop();
+            }
+            rank++;
+        }
+
+        return ans;
     }
 };
