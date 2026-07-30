4197209c5d3105d8ff4ece95f2bb97e0f9b4f604
Diptisingh007 <dipptisingh001@gmail.com>
2026-07-30 14:31:28 +0530

    Time: 0 ms (100%), Space: 12.7 MB (25.34%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,int x,int y){
+    int solve(vector<int> &nums,int x,int y){
         int n=nums.size();
-        vector<int>dp1(n,0),dp2(n,0);
+        vector<int> first(n,0),second(n,0);
         int sum=0;
+
         for(int i=0;i<n;i++){
-            if(i<x){//when we haven't considered x-size array
-                sum+=nums[i]; 
-                dp1[i]=sum;
+            if(i<x){
+                sum=sum+nums[i];
+                first[i]=sum;
             }
-            else{ //when we have a window of size x
+            else{
                 sum=sum+nums[i]-nums[i-x];
-                dp1[i]=max(dp1[i-1],sum);
+                first[i]=max(sum,first[i-1]);
             }
         }
+
         sum=0;
-        //check from right to left for Secondlen window and store maximum sum from ith index to last index keep updating at every point if the current window of size secondLen is max or dp2[i+1] has max whichever is max add that to dp[i]
-        
         for(int i=n-1;i>=0;i--){
-            if(i+y>n-1) { //case when we haven't encountered a window of size secondlen ie. y
-                sum+=nums[i];
-                dp2[i]=sum;
+            if(i+y>n-1){
+                sum=sum+nums[i];
+                second[i]=sum;
             }
-            else{//when we have a window of size secondLen
+            else{
                 sum=sum+nums[i]-nums[i+y];
-                dp2[i]=max(dp2[i+1],sum);
+                second[i]=max(second[i+1],sum);
             }
         }
-        int ans=0;
-        //our ans window will be from x-1 to n-y
+
+        int ans=INT_MIN;
         for(int i=x-1;i<n-y;i++){
-            ans=max(ans,dp1[i]+dp2[i+1]);
+            ans=max(ans,first[i]+second[i+1]);
         }
+
         return ans;
     }
-    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen){
-        return max(helper(nums, firstLen, secondLen),helper(nums, secondLen, firstLen));
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        return max(solve(nums,firstLen,secondLen),solve(nums,secondLen,firstLen));
     }
 };
