304fd32af64bfe953cc7953ff5f1564d019db893
Diptisingh007 <dipptisingh001@gmail.com>
2026-08-02 18:15:11 +0530

    Time: 0 ms (100%), Space: 11.7 MB (51.05%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -20,7 +20,8 @@ public:
             for(int i=0;i<n;i++){
                 int sum=0;
                 int j=i-1;
-                for(int p=k;p<0;p++){
+                k=abs(k);
+                for(int p=0;p<k;p++){
                     sum=sum+code[(j+n)%n];
                     j--;
                 }
