db838e72c7b3f16b467b5a2ffb526ed3ea7fa225
Diptisingh007 <dipptisingh001@gmail.com>
2026-08-13 10:36:53 +0530

    Time: 7 ms (65.07%), Space: 38.2 MB (96.34%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,59 +1,23 @@
 class Solution {
 public:
-    // int leastInterval(vector<char>& tasks, int n) {
-    //     int m=tasks.size();
-    //     unordered_map<char,int> mpp;
-    //     for(auto &it: tasks){
-    //         mpp[it]++;
-    //     }
-
-    //     priority_queue<int> pq;
-    //     for(auto &it: mpp) pq.push(it.second);
-        
-    //     int ans=0;
-    //     while(!pq.empty()){
-    //         vector<int> temp;
-    //         for(int i=0;i<n+1;i++){
-    //             if(!pq.empty()){
-    //                 int freq=pq.top();
-    //                 pq.pop();
-    //                 freq--;
-    //                 temp.push_back(freq);
-    //             }
-    //         }
-
-    //         for(auto &it: temp){
-    //             if(it>0) pq.push(it);
-    //         }
-
-    //         if(!pq.empty()){
-    //             ans+=(n+1);
-    //         }
-    //         else{
-    //             ans+=temp.size();
-    //         }
-    //     }
-    //     return ans;
-    // }
-
-
-
     int leastInterval(vector<char>& tasks, int n) {
         int m=tasks.size();
-        vector<int> freq(26,0);
+        unordered_map<char,int> mpp;
         int maxi=0;
-        for(int i=0;i<m;i++){
-            freq[tasks[i]-'A']++;
-            maxi=max(maxi,freq[tasks[i]-'A']);
+        for(auto &it: tasks){
+            mpp[it]++;
+            maxi=max(maxi,mpp[it]);
         }
-         
-        int temp=0;
-        for(auto &it:freq){
-            if(it==maxi) temp++;
-        } 
 
-        int ans=max(m,(maxi-1)*(n+1)+temp);
-        
+        int cnt=0;
+        for(auto &it: mpp){
+            if(it.second==maxi){
+                cnt++;
+            }
+        }
+        int ans=max(m,(maxi-1)*(n+1)+ cnt);
         return ans;
+
+
     }
 };
