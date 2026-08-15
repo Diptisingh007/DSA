1fc53e6152b9a413d4a9b01bb55cecf48452c90b
Diptisingh007 <dipptisingh001@gmail.com>
2026-08-15 16:35:35 +0530

    Time: 3 ms (82.09%), Space: 13.6 MB (84.1%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -4,8 +4,8 @@ public:
         int n=s.size();
         string ans="";
 
-        ans=ans+s[0];
-        for(int i=1;i<n;i++){
+        // ans=ans+s[0];
+        for(int i=0;i<n;i++){
             if( !ans.empty() && ans.back()==s[i]){
                 ans.pop_back();
             }
